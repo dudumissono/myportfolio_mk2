f7fc25f3599e242e3f3c86ce552198d2f4a930fa
Dudu Missono <dudumissono@gmail.com>
2022-10-04 14:24:13 -0400

    started the projects page and some minor updates in navbar

--- app/controllers/projects.server.controller.js
+++ app/controllers/projects.server.controller.js
@@ -13,6 +13,16 @@ exports.render = function (req, res) {
 
     res.render('projects', {
         title: 'Projects',
-        description: 'Mr. Anderson'
+        description: 'As my tech career is just beginning, I decided to list here on tech job and two other projects that I’ve worked on before.',
+        projectOne: {
+            title: 'Software Development',
+            img: '',
+            description: 'Part-time internship in Canada. I’m working on a project that was built using low-code tools, but I’m able to exercise programming logic and write some custom code inside the software.'
+        },
+        projectTwo: {
+            img: '',
+            description: ''
+        },
+        references: ''
     })
 };
