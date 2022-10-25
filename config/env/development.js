9924f008e487c5573b063aa68977b9843acedff6
Dudu Missono <dudumissono@gmail.com>
2022-10-25 18:33:06 -0400

    mongodb atlas configured

--- config/env/development.js
+++ config/env/development.js
@@ -6,5 +6,6 @@ Date: Oct 03, 2022
 
 module.exports = {
     sessionSecret: 'developmentSessionSecret',
-    db: 'mongodb://localhost/mean-comp229'
+    // db: 'mongodb://localhost/mean-comp229',
+    db: 'mongodb+srv://emissono:V9GGlHMQBBAsJxYv@mongodbserver.b5zcdkt.mongodb.net/portfolio?retryWrites=true&w=majority'
 }
--- config/mongoose.js
+++ config/mongoose.js
@@ -8,8 +8,18 @@ const config = require('./env/development');
 const mongoose = require('mongoose');
 
 module.exports = function () {
-    const db = mongoose.connect(config.db);
-    require('../app/models/contact.server.model');
+    const db = mongoose.connect(config.db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => {
+        console.log('MongoDb Connection Successful');
+    })
+        .catch((error) => {
+            console.log(error);
+        })
 
+    require('../app/models/contact.server.model');
+    require('../app/models/user.server.model');
     return db;
 }
